14a783d9fb81fe1a37183a8d7278fdb0e69e7d8d
amil1105 <51198499+amil1105@users.noreply.github.com>
2024-12-24 16:39:45 +0300

    Add files via upload

---
+++ increment.c
@@ -0,0 +1,35 @@
+//AMIL SHIKHIYEV G221210561
+//Erkin Erdoğan B241210385
+//Kianoush Seddighpour G221210571
+//Manar AL SAYED ALI G221210558
+
+// increment.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("increment running in %s\n", cwd);
+    }
+    else {
+        perror("getcwd() error");
+    }
+
+    int number;
+
+    // Standart girdiden bir tam sayı oku
+    if (scanf("%d", &number) != 1) {
+        fprintf(stderr, "Hata: Bir tam sayı girmelisiniz.\n");
+        return 1;
+    }
+
+    // Tam sayıyı bir artır
+    number += 1;
+
+    // Sonucu standart çıktıya yaz
+    printf("%d\n", number);
+
+    return 0;
+}
